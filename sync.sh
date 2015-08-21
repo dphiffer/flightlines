bcab550fa7c48c8f12368e8f01c82907f3e345a2
Dan Phiffer <dan@phiffer.org>
2015-08-21 14:27:38 -0400

    Change directory before git pull

--- sync.sh
+++ sync.sh
@@ -8,6 +8,6 @@ logfile="$basedir/logs/$location-sync-$date.log"
 {
 	echo "-- $date $time --"
 	rsync -rv $basedir/videos/$location flserver:/home/flightlines/
-	git pull origin master -q
+	cd $basedir && git pull origin master -q
 } >> $logfile
 rsync -r --exclude .keep-dir $basedir/logs/ flserver:/home/flightlines/$location/
