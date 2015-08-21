90542a4cf7ef523da947abbc7ead2f8d3545e48d
Dan Phiffer <dan@phiffer.org>
2015-08-21 14:29:00 -0400

    Comments

--- sync.sh
+++ sync.sh
@@ -7,7 +7,13 @@ time=`date +%H:%M:%S`
 logfile="$basedir/logs/$location-sync-$date.log"
 {
 	echo "-- $date $time --"
+
+	# Sync video files
 	rsync -rv $basedir/videos/$location flserver:/home/flightlines/
+
+	# Update scripts
 	cd $basedir && git pull origin master -q
 } >> $logfile
+
+# Sync log files
 rsync -r --exclude .keep-dir $basedir/logs/ flserver:/home/flightlines/$location/
