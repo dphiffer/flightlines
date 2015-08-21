bb2780f1cf2db367b1c3d6acea10c8a07c492797
Dan Phiffer <dan@phiffer.org>
2015-08-21 15:19:06 -0400

    Process MP4 video

--- capture.sh
+++ capture.sh
@@ -8,7 +8,7 @@ min_time="55959"  # start after 05:59:59
 max_time="200000" # end before 20:00:00
 
 if [ ! -d "$videos" ]; then
-	`mkdir -p $videos`
+	mkdir -p $videos
 fi
 
 while [ 1 ]
@@ -21,19 +21,22 @@ do
 	{
 		if (( $comp_time > $min_time )) && (( $comp_time < $max_time )); then
 
-			filename="$location-$date-$time.mp4"
-			echo "$filename"
+			h264_file="$location-$date-$time.h264"
+			mp4_file="$location-$date-$time.mp4"
+			echo $h264_file
 
 			# Capture video for 10 minutes
-			`raspivid -t 600000 -n -w 960 -h 540 -b 12500000 -o $basedir/$filename`
+			raspivid -t 600000 -n -w 960 -h 540 -b 12500000 -o $basedir/$h264_file
 
 			# Create the date folder if none exists
 			if [ ! -d "$videos/$date" ]; then
-				`mkdir -p $videos/$date`
+				mkdir -p $videos/$date
 			fi
 
-			# Move mp4 to videos folder
-			`mv $basedir/$filename $videos/$date/$filename`
+			# Process mp4 file and move it to the videos folder
+			MP4Box -add $basedir/$h264_file $basedir/$mp4_file
+			mv $basedir/$mp4_file $videos/$date/$mp4_file
+			rm $basedir/$h264_file
 
 		else
 
