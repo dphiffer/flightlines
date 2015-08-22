cb9ac118ee2e8248ddd92f72d72aa8bf1aed0b85
Dan Phiffer <dan@phiffer.org>
2015-08-22 08:14:43 -0400

    Tweaks

--- start.sh
+++ start.sh
@@ -8,11 +8,5 @@ basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # Update scripts
 cd "$basedir" && git pull origin master -q
 
-# Cleanup in-progress video files
-for file in $basedir/*.h264 ; do
-	if [ -e "$basedir/$file" ] ; then
-		rm "$basedir/$file"
-	fi
-done
-
+# Start capture script
 `$basedir/capture.sh`
--- stop.sh
+++ stop.sh
@@ -3,7 +3,15 @@
 # Cleanup lock files
 rm /tmp/flightlines*
 
+# Stop any existing processes
 killall capture.sh
 killall sync.sh
 killall rsync
 killall raspivid
+
+# Cleanup in-progress video files
+for file in $basedir/*.h264 ; do
+	if [ -e "$basedir/$file" ] ; then
+		rm "$basedir/$file"
+	fi
+done
