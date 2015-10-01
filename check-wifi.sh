cd10f4e3e25fbd30cf9c85ae0c4b04712708f8e3
Dan Phiffer <dan@phiffer.org>
2015-10-01 09:29:35 -0400

    Kill sync.sh if wifi goes down

--- check-wifi.sh
+++ check-wifi.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ping -c4 phiffer.org > /dev/null
- 
-if [ $? != 0 ] 
-then
+
+if [ $? != 0 ] ; then
 	/sbin/ifdown 'wlan0'
+	/usr/bin/killall sync.sh
+	/bin/rm /tmp/flightlines-sync.lock
 	sleep 5
 	/sbin/ifup --force 'wlan0'
 fi
