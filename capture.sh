0d228ff24c79b65353c5fd273545e41fcbad8ec7
Dan Phiffer <dan@phiffer.org>
2015-12-15 09:12:47 -0500

    👔 New lockfile stuff

--- capture.sh
+++ capture.sh
@@ -1,56 +1,60 @@
 #!/bin/bash
 
-min_time="90000"    # start at 9am
-max_time="220000"   # end at 10pm
-
 lockfile="/tmp/flightlines-capture.lock"
-basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-location="nowhere"
+if ( set -o noclobber; echo "locked" > "$lockfile") 2> /dev/null; then
+	
+	trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
 
-# Don't run if stopped
-if [ -f "$basedir/stopped" ] ; then
-	exit 1
-fi
+	min_time="90000"    # start at 9am
+	max_time="230000"   # end at 11pm
 
-if [ -f "/etc/hostname" ] ; then
-	location=`cat /etc/hostname`
-fi
+	basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+	location="nowhere"
 
-videos="$basedir/videos/$location"
-
-# Override timeout with first param
-if [ -n "$1" ] ; then
-	timeout="$1"
-fi
-
-if [ ! -d "$videos" ] ; then
-	mkdir -p "$videos"
-fi
-
-date=`date +%Y%m%d`
-time=`date +%H%M%S`
-log_date=`date +%Y-%m-%d`
-comp_time=`echo $time | sed 's/^0*//'`
-logfile="$basedir/logs/$location-capture-$log_date.log"
-{
-	if (( $comp_time > $min_time )) && (( $comp_time < $max_time )) ; then
-
-			jpg_file="$location-$date-$time.jpg"
-			echo "$jpg_file"
-			
-                        # Create the date folder if none exists
-			if [ ! -d "$videos/$date" ] ; then
-				mkdir -p "$videos/$date"
-			fi
-			# Capture video
-			raspistill \
-			    --output "$videos/$date/$jpg_file" \
-			    --width 1920 \
-			    --height 1440 \
-			    --nopreview \
-			    --quality 70
-
+	# Don't run if stopped
+	if [ -f "$basedir/stopped" ] ; then
+		exit 1
 	fi
-} >> $logfile
+
+	if [ -f "/etc/hostname" ] ; then
+		location=`cat /etc/hostname`
+	fi
+
+	videos="$basedir/videos/$location"
+
+	if [ ! -d "$videos" ] ; then
+		mkdir -p "$videos"
+	fi
+
+	date=`date +%Y%m%d`
+	time=`date +%H%M%S`
+	log_date=`date +%Y-%m-%d`
+	comp_time=`echo $time | sed 's/^0*//'`
+	logfile="$basedir/logs/$location-capture-$log_date.log"
+	{
+		if (( $comp_time > $min_time )) && (( $comp_time < $max_time )) ; then
+
+				jpg_file="$location-$date-$time.jpg"
+				echo "$jpg_file"
+				
+				# Create the date folder if none exists
+				if [ ! -d "$videos/$date" ] ; then
+					mkdir -p "$videos/$date"
+				fi
+				# Capture video
+				raspistill \
+				    --output "$videos/$date/$jpg_file" \
+				    --width 1920 \
+				    --height 1440 \
+				    --nopreview \
+				    --quality 70
+
+		fi
+	} >> $logfile
+	
+	# Release lockfile
+	rm -f "$lockfile"
+	
+fi
 
 `$basedir/sync.sh`
--- sync.sh
+++ sync.sh
@@ -1,51 +1,56 @@
 #!/bin/bash
 
 lockfile="/tmp/flightlines-sync.lock"
-basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-location="nowhere"
-
-# Don't run if stopped
-if [ -f "$basedir/stopped" ] ; then
-	exit 1
-fi
-
-if [ -f "/etc/hostname" ] ; then
-	location=`cat /etc/hostname`
-fi
-
-date=`date +%Y-%m-%d`
-time=`date +%H:%M:%S`
-logfile="$basedir/logs/$location-sync-$date.log"
-
-# Don't run more than one sync script at a time
-if [ -f "$lockfile" ] ; then
-	echo "Lock file exists: $lockfile"
-	exit 1
-fi
-
-touch "$lockfile"
-
-{
-	echo "-- $date $time --"
-
-	# Sync video files to server
-	rsync \
-		--recursive \
-		--verbose \
-		--ignore-existing \
-		--remove-source-files \
-		"$basedir/videos/$location" \
-		flserver:/home/flightlines/videos/
-
-	# Update scripts
-	cd "$basedir" && git pull origin plantcam -q
+if ( set -o noclobber; echo "locked" > "$lockfile") 2> /dev/null; then
 	
+	trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
+
+	basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+	location="nowhere"
+
+	# Don't run if stopped
+	if [ -f "$basedir/stopped" ] ; then
+		exit 1
+	fi
+
+	if [ -f "/etc/hostname" ] ; then
+		location=`cat /etc/hostname`
+	fi
+
 	date=`date +%Y-%m-%d`
 	time=`date +%H:%M:%S`
-	echo "Finished at $date $time"
-} >> "$logfile"
+	logfile="$basedir/logs/$location-sync-$date.log"
 
-# Sync log files
-rsync -r --exclude .keep-dir "$basedir/logs/" "flserver:/home/flightlines/videos/$location/logs/"
+	{
+		echo "-- $date $time --"
 
-rm "$lockfile"
+		# Sync video files to server
+		rsync \
+			--recursive \
+			--verbose \
+			--ignore-existing \
+			--remove-source-files \
+			--timeout=30 \
+			"$basedir/videos/$location" \
+			flserver:/home/flightlines/videos/
+
+		# Update scripts
+		cd "$basedir" && git pull origin plantcam -q
+		
+		date=`date +%Y-%m-%d`
+		time=`date +%H:%M:%S`
+		echo "Finished at $date $time"
+	} >> "$logfile"
+
+	# Sync log files
+	rsync \
+		--recursive \
+		--exclude .keep-dir \
+		--timeout=30 \
+		"$basedir/logs/" \
+		"flserver:/home/flightlines/videos/$location/logs/"
+
+	# Release lockfile
+	rm -f "$lockfile"
+
+fi
