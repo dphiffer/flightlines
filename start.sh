f6ed030c95da74a8b756c031fbad37ca1b9b6d0e
Dan Phiffer <dan@phiffer.org>
2016-01-21 09:34:59 -0800

    🐣 Log start script

--- start.sh
+++ start.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+location="nowhere"
+log_date=`date +%Y-%m-%d`
+
+if [ -f "/etc/hostname" ] ; then
+	location=`cat /etc/hostname`
+fi
 
 # Stop any scripts still running
 `$basedir/stop.sh restarting`
 
-# Update scripts
-cd "$basedir" && git pull origin master -q
+logfile="$basedir/logs/$location-start-$log_date.log"
+{
+	ifconfig
 
-# Remove stopped flag
-if [ -f "$basedir/stopped" ] ; then
-	rm "$basedir/stopped"
-fi
+	# Update scripts
+	cd "$basedir" && git pull origin master -q
+
+	# Remove stopped flag
+	if [ -f "$basedir/stopped" ] ; then
+		rm "$basedir/stopped"
+	fi
+} >> $logfile
 
 `$basedir/capture.sh`
