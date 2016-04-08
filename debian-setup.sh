55ad1aedb00630fec1de7a68e02d32b40fb51b02
Dan Phiffer <dan@phiffer.org>
2016-04-08 15:41:44 -0400

    Hide non-viewer content

---
+++ debian-setup.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y apache2 php5 php5-mysql mysql-server emacs24-nox git lightdm xinit iceweasel
+git clone https://github.com/dphiffer/flightlines.git
+cd flightlines
+git checkout fullscreen
